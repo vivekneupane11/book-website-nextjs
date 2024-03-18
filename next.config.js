661c050a24c23f373fe1c01064965aeb43d2901c
iamjesusc <d.fox.fedra@gmail.com>
2024-03-18 09:53:39 -0500

    Addd next.js image configuration.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+           {
+            protocol: 'https',
+            hostname: "*",
+            port: '',
+           },
+        ] 
+    }
+}
 
 module.exports = nextConfig
